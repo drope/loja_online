94ffcaef4f6a1efe8d37ad0df6f35204893adb49
Pedro Son <pedro.son@gmail.com>
2011-02-20 01:33:40 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+gem 'pg'
+
+group :development do 
+	gem 'rspec-rails'
+end
+
+group :test do 
+	gem 'rspec'
+	gem 'webrat'
+end
+
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LojaOnline::Application.config.secret_token = '05b9326fb265c99cec2d2562eddcc780039c189a75ac7f1e0b6be22ecb76de1462389b28df179556d7de3f08a3de07700c5f73496144d7bd9899dae73cee9926'
